b9c06070aff4bf0e93326c3900342f99bc4a6bc3
lokajaya1 <lokajayandala1@gmail.com>
2024-09-20 05:17:06 +0700

    add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Hello&apos;w</h1>
-      <h2 className="h2-bold">Hello&apos;w</h2>
-      <h3 className="h3-bold">Hello&apos;w</h3>
-    </div>
-  );
-}
