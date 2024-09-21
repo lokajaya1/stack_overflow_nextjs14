15f1ca4ce3fcc8da22a58cf247154a8480afa6c2
lokajaya1 <lokajayandala1@gmail.com>
2024-09-21 18:54:46 +0700

    implement theme provider

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
+// import { useTheme } from "@/context/ThemeProvider";
 
 export default function Home() {
+  // const { mode } = useTheme();
   return (
     <div>
-      <UserButton />
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
