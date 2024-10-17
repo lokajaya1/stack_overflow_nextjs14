65e2f944f522724971eb1a7cb719a5a88e97177e
lokajaya1 <lokajayandala1@gmail.com>
2024-10-17 14:17:12 +0700

    modified UserCard hydration

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { HomePageFilters } from "@/constants/filter";
+import { HomePageFilters } from "@/constants/filters";
 import React from "react";
 import { Button } from "../ui/button";
 
