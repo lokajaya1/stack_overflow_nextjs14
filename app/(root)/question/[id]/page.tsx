021aa186da1bd778a4757e4ffadc6149a4068f94
Lokajaya Andala <lokajayandala1@gmail.com>
2024-10-30 18:57:01 +0700

    integrated filter answers

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -101,6 +101,8 @@ const Page = async ({ params, searchParams }: any) => {
         questionId={result.id}
         userId={mongoUser._id}
         totalAnswers={result.answers.length}
+        page={searchParams?.page}
+        filter={searchParams?.filter}
       />
 
       <Answer
