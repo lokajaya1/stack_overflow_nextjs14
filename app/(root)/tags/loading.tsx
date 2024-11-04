3b5c30e5512164cdea257a6f74ce3f05ba477d29
Lokajaya Andala <lokajayandala1@gmail.com>
2024-11-05 01:42:25 +0700

    Create Loading states for the rest of the pages

--- app/(root)/tags/loading.tsx
+++ app/(root)/tags/loading.tsx
@@ -3,15 +3,11 @@ import { Skeleton } from "@/components/ui/skeleton";
 const Loading = () => {
   return (
     <section>
-      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
-        <h1 className="h1-bold text-dark100_light900">All Users</h1>
-      </div>
+      <h1 className="h1-bold text-dark100_light900">Tags</h1>
 
-      <div className="mb-12 mt-11 flex flex-wrap gap-5">
+      <div className="mb-12 mt-11 flex flex-wrap items-center justify-between gap-5">
         <Skeleton className="h-14 flex-1" />
-        <div className="hidden max-md:block">
-          <Skeleton className="h-14 w-28" />
-        </div>
+        <Skeleton className="h-14 w-28" />
       </div>
 
       <div className="flex flex-wrap gap-4">
